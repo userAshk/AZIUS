3ae20ac02e129e27042ecfb730a4e03371701b1b
userAshk <blvckm8@gmail.com>
2021-11-21 19:18:50 +0300

    scrolling features

---
+++ js/app.js
@@ -0,0 +1,39 @@
+function scrollTo(element) {
+    window.scroll({
+      behavior: 'smooth',
+      left: 0,
+      top: element.offsetTop - 80
+    });
+    event.preventDefault();
+}
+
+//ABOUT
+document.getElementById("about-btn").addEventListener('click', () => {
+    scrollTo(document.getElementById("about"));
+});
+
+//SERVICES
+document.getElementById("serv-btn").addEventListener('click', () => {
+    scrollTo(document.getElementById("services"));
+});
+
+//PERTNERS
+document.getElementById("partners-btn").addEventListener('click', () => {
+    scrollTo(document.getElementById("partners"));
+});
+
+//CONTACTS
+document.getElementById("contacts-btn").addEventListener('click', () => {
+    scrollTo(document.getElementById("contacts"));
+});
+
+
+var scrollTrigger = 60;
+
+window.onscroll = function() {
+  if (window.scrollY >= scrollTrigger || window.pageYOffset >= scrollTrigger) {
+    document.getElementsByClassName("header")[0].classList.add('header-bg');
+  } else {
+    document.getElementsByClassName("header")[0].classList.remove('header-bg');
+  }
+};
