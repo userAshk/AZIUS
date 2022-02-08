5135427adb3b7cf321346426ebdb94da006caef7
Achekeev <achekeev88@gmail.com>
2022-02-08 14:59:41 +0600

    Changed pics and some words

--- js/langSwitcher.js
+++ js/langSwitcher.js
@@ -2,7 +2,7 @@ var arrLang = {
     'ru': {
         //HEADER
         'about': 'О нас',
-        'services': 'Наши сервисы',
+        'services': 'Сферы Деятельности',
         'partners': 'Партнеры',
         'contact': 'Контакты',
 
@@ -32,7 +32,7 @@ var arrLang = {
     'en': {
         // HEADER
         'about': 'About Us',
-        'services': 'Our services',
+        'services': 'Areas of Activity',
         'partners': 'Our partners',
         'contact': 'Contacts',
 
