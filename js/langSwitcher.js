05a3536c60267391db224666bd493023f2d7a996
userAshk <blvckm8@gmail.com>
2022-02-09 01:06:18 +0300

    multilang in proccess

--- js/langSwitcher.js
+++ js/langSwitcher.js
@@ -29,17 +29,36 @@ var arrLang = {
 
         //MODALS
         'timber':'Лесоматериалы',
+        't1':'Наши лесоматериалы привозятся из России, а именно c легендарных лесов вдоль реки Ангара. Ценная ангарская сосна отличается твердостью и плотностью. Такая древесина с легкостью переносит перепады в температуре и скачков влажности, благодаря суровому сибирскому климату. Другая примечательная черта такой древесины - ее пластичность, что значительно упрощает ее обработку.',
+        't2':'Наша компания предлагает круглые бревна с корой и доски обрезные I и II сортов. Наши пиломатериалы из-под многопильного станка (дисковое пиление) создаются с техникой выдержанной геометрии для стабильного и предсказуемого результата. С таким вниманием к деталям мы добиваемся четких линий, идеальных пропорций и других желаемых исходов.',
+        't3':'Довольно часто лесопильные заводы могут предоставить неправильные размеры, куда меньше запрошенных, что повышает их прибыль. С этим повышается количество недовольных клиентов. Наш производственный процесс построен на строгих стандартах качества и требованиях клиента. Мы ориентируемся на благоприятный и эффективный опыт работы с клиентом.',
+        't4':'На данный момент нашими клиентами являются горнорудные и строительные компании.',
 
+        'metalHEad':'МЕТАЛЛИЧЕСКИЕ КРЕПИ',
+        'mt1':'Часто металлические крепи встречаются в работе горной промышленности. Такие сооружения используют для работы в горно-геологических условиях. Благодаря проверенной прочности, огнестойкости и долговечности металлические крепи идеально подойдут для работ в местности с высоким горным давлением. ',
+        'mt2':'Такая металлическая крепь представляет специальные сооружения из рам и соединительных узлов. Ее преимущество перед деревянными крепями в том, что металлические крепи имеют высокие эксплуатационные качества и может прослужить долгие годы. ',
 
         'honeyHead':'МЁД “AZIUS”',
         'nature':'Уникальный. Сырой. Натуральный',
         'premium':'“Azius” - премиальный натуральный мёд, собранный на горных склонах Центральной Азии. Благодаря нашей приверженности ценностям исключительного качества, мы создали первоклассный продукт, объединивший в себе уникальность и лечебные свойства высокогорных медоносных растений.',
+        'f2':'Мед отличается многогранностью своего вкуса. Он раскрывается новыми нотами и неожиданными нюансами в самых разных условиях будь то на свежем тосте или в чашке горячего чая. Такой контроль и гарантия высокого качества каждой баночки возможен благодаря владению собственными пасеками. Этический сбор меда и фасовка сохраняют природные витамины и полезные микроэлементы.',
+        'f3':'Наш бренд пользуется спросом на международных рынках, включая ОАЭ, Южную Корею и Китай. Так наша продукция оказалась на полках крупного ритейлера в ОАЭ с более чем 100 точек на Ближнем Востоке.',
 
-        'key':'value',
-        'key':'value',
-        'key':'value',
-        'key':'value',
-        'key':'value',
+        'jams':'ДЖЕМЫ',
+        'j1':'Джемы “Azius” запоминаются не только своими насыщенными вкусами, но и превосходным качеством. Покупая баночку джема, вы получаете настоящий органический продукт.',
+
+        'malalacto':'БИОЛАКТ “MAMALACTO”',
+        'malo1':'Биолакт “MAMALACTO” дуален в своих функциях: он может быть вкусным напитком, но при этом насытить полезными бактериями. Этот уникальный продукт был создан кыргызскими учеными в Советском Союзе. Мы переняли наследие наших предшественников и применяем те же этические технологии производства, что в свою очередь позволяет убедиться в качестве продукта.',
+        'malo2':'Этот биологически активный кисломолочный продукт напоминает сметану с легкой кислотностью и сладостью. С советских времен биолакт использовали для укрепления иммунитета. Его лечебные и профилактические свойства повышают защитные реакции организма и положительно влияют на физическое и умственное здоровье человека.',
+        'malo3':'В производстве мы используем только молоко отборных и здоровых коров. За основу биолакта берутся закваски молочнокислых бактерий с высокими антибиотическими свойствами для задержки роста дизентерийной и тифозной палочек, стафилококков, сарцин, бактерий протея, продигиозут, сенной и кишечной палочек. Способность биолакта расщеплять белки, жиры и углеводы делают его легко усвояемым для организма. Он также эффективен для предупреждения острых кишечных инфекций и снижения побочных действий антибиотиков.',
+        'malo4':'“MAMALACTO” может стать настоящим семейным продуктом. Различные ферменты, свободные аминокислоты, витамины и другие ингредиенты значительно повышают питательную ценность, что улучшает обмен веществ и нормализует секреторную функцию желудка и кишечной микрофлоры при любом возрасте. ',
+
+        'juiceHead':'СОКИ ПРЯМОГО ОТЖИМА AZIUS',
+        'jc1':'“AZIUS” - лучший в своем классе бренд натуральных соков. В производстве используется перворазрядное голландское оборудование и методы прямого отжима. Это в свою очередь доказывает отсутствие концентратов и консервантов.',
+        'jc2':'В нашем продукте не используется ГМО и соответственно наш продукт соответствует всем международным стандартам качества, аутентичность и элегантная простота AZIUS остаются краеугольным камнем нашей философии.   ',
+        'jc3':'Выращенные в Ноокатском районе Кыргызстана яблоки и абрикосы имеют поразительно насыщенный вкус, что создает идеальную базу для всех соков. Благодаря тесным и плодотворным отношениям с местными фермерами мы получаем только качественные ингредиенты, которые отвечают всем запросам органического агрохозяйства.',
+
+
         'key':'value',
     },
 
@@ -74,15 +93,37 @@ var arrLang = {
         //MODALS
 
         'timber':'Timber',
+        't1':'Our timber comes from Russia’s legendary forests along the river of Angara. Valuable Angara pine is particularly known for its density and durability. It easily endures changes in temperature and rising humidity thanks to the severe Siberian climate. Another note-worthy feature of the timber is the elasticity that simplifies the treatment. ',
+        't2':'Our company offers round logs and edged boards (I and II grades). Lumber from under the multi-saw machine (disc sawing) is executed with a consistent geometric technique for a stable and predictable result. Attention to details helps us achieve impeccable lines, perfect proportions and other desirable outcomes.',
+        't3':'Too often sawmills can produce timber of inaccurate sizes, far less than what’s been requested, which increases their revenue. This results in an astounding number of angry customers. Our production process is built on strict standards of quality and customer service. We strive to create a great experience for everyone involved. ',
+        't4':'Currently we are supplying timber to mining and construction companies.',
+
+
+        'metalHEad':'METAL SUPPORTS',
+        'mt1':'Metal supports are often met in the mining industry. Such structures are used in geological conditions. Their proven density, fire resistance and durability makes them perfect for any work in high altitude areas. ',
+        'mt2':'A single metal support looks like a special structure made out of brackets and connecting nodes. Its advantage over wooden supports is that they have high exploitive qualities and can last for many years.',
 
         'honeyHead':'Honey "AZIUS"',
         'nature':'Unique. Raw. Natural',
         'premium':'“Azius” is a premium brand of natural honey collected on the foothills of Central Asia. Committed to values of exceptional quality we created a first-class product that blends the uniqueness and medicinal features of mountain honey plants. ',
-        'key':'value',
-        'key':'value',
-        'key':'value',
-        'key':'value',
+        'f2':"The honey stands out thanks to its rich and complex taste. It surprises with new notes and unexpected nuances at every meal be it on a fresh toast or in a hot cup of tea. Such control and guarantee of high quality in each jar is only possible with owning apiaries. This allows us to ethically collect and pre-pack honey so that the natural vitamins and nutritional microelements don't evaporate in the process.",
+        'f3':'The brand has been successful at capturing the attention of loyal customers in international markets, including UAE, South Korea and China. Currently “Azius” is on the shelves of a large UAE retailer with over 100 stores across the Middle East. ',
 
+        'jams':'JAMS',
+        'j1':'“Azius” jams are produced with deep flavors and excellent quality at its core. With every jar you get an authentic natural product. Technologies of chemical-free production could be felt with each spoon of the jam. We don’t add any sugar and instead opt for a natural sweetener - honey.',
+
+        'malalacto':'“MAMALACTO” BIOLACT',
+        'malo1':'“MAMALACTO” biolact is dual in its functionality: it can be a tasty treat, but also be a much needed fill of useful bacteria. This unique product was developed by Kyrgyz scientists in the Soviet Union. We proudly use the legacy of our predecessors to adapt their innovation in ethical technologies to ensure the highest of qualities in production. ',
+        'malo2':'This biologically active fermented milk product reminds of a sour cream with slight acidity and sweetness. It has been used as a remedy and an immune system boost since Soviet times. It’s medicinal and preventive properties strengthen protective reactions of the body and positively affect physical and mental health of a person.',
+        'malo3':'We source milk from selected and healthy cows for production use. At its basis biolact is a ferment of lactic acid bacteria with high antibiotic properties. It is often used to delay the growth of dysentery and typhoid bacilli, staphylococci, sarcine, Proteus bacteria, hay and Escherichia coli. Biolact’s ability to break down proteins, fats and carbohydrates makes it easy to digest. It is also effective for prevention of acute intestinal infections and side effects of antibiotics.',
+        'malo4':'“MAMALACTO” can be a truly family product. Various ferments, free amino acids, vitamins and other ingredients significantly increase its nutritional value which improves digestion and normalises secretory functions of the stomach and intestinal microflora at any age.  ',
+
+        'juiceHead':'Directly squeezed juices AZIUS',
+        'jc1':'“AZIUS” is “best in class” when it comes to the additive-free juice category. Production uses first-class Dutch equipment and direct squeeze technology not from concentrate. This in turn proves the absence of concentrates, conservants.',
+        'jc2':'We don’t use GMO in any of our products to comply with international standards of quality. Authenticity and elegant simplicity of AZIUS are still the foundation of our philosophy.',
+        'jc3':'Peaches and apples harvested in the Nookat district of Kyrgyzstan have a remarkably rich taste which creates a perfect base for all of our juices. Thanks to close relationships with local farmers we only get great ingredients that meet the requirements of organic agriculture. ',
+        
+        'key':'value',
     },
 }
 
